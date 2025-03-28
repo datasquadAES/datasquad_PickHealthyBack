60d8497639e3e38dfdc6fcaee0b85ffbc46ab986
erazo <erazoo@javeriana.edu.co>
2025-03-28 07:27:33 -0500

    🔧 Add endpoint to retrieve Google Maps API key

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const app = require('./index.js');
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/api-key', (req, res) => {
+  res.send(process.env.GOOGLE_MAPS_API_KEY);
+});
+
 // Middleware
 app.use(express.json());
 
